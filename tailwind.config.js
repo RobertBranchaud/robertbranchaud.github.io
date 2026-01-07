1e7ea0a6919c30ffb438c332eb6880a16d29d0c2
bobert <bobbybranchaud360@gmail.com>
2026-01-07 13:47:06 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./projects.html",
+    "./resume.html",
+    "./about.html",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
